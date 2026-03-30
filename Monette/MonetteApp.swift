4553820a6c3cc4e62655e27c3ae3b16cfa9a075e
Erik Dao <cuong@neuraltalks.io>
2026-03-30 12:04:17 +0200

    Initial Commit

---
+++ Monette/MonetteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonetteApp.swift
+//  Monette
+//
+//  Created by Bach on 2026-03-30.
+//
+
+import SwiftUI
+
+@main
+struct MonetteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MonetteTests/MonetteTests.swift
@@ -0,0 +1,19 @@
+//
+//  MonetteTests.swift
+//  MonetteTests
+//
+//  Created by Bach on 2026-03-30.
+//
+
+import Testing
+@testable import Monette
+
+struct MonetteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
